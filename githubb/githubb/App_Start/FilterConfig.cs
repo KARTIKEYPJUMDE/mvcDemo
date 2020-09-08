963aba2fe029502dcab6dcbd823a21f7f722566e
Kartikey P Jumde <dxc32dnb2041@datascience.manipal.edu>
2020-09-08 15:10:41 +0530

    added employee controller

---
+++ githubb/githubb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace githubb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
